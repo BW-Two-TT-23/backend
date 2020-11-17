bee0335b7f14481e08e5ae3bab72c76463b32847
Christopher Barrett <municipalshock@gmail.com>
2020-11-16 16:00:01 -0800

    class router

---
+++ api/api-router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userRouter = require('../users/user-router');
+const authRouter = require('../auth/auth-router');
+const classRouter = require('../class/class-router');
+
+
+router.use('/auth', authRouter);
+router.use('/users', userRouter);
+router.use('/class', classRouter);
+
+
+module.exports = router;
---
+++ class/class-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+  find,
+  findByType,
+  findById,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db('class');
+}
+
+function findByType(type) {
+  return db('class').where({ type });
+}
+
+function findById(id) {
+  return db('class')
+    .where({ id })
+    .first();
+}
+
+function add(post) {
+    return db('class')
+      .insert(post)
+      .then(ids => {
+        return findById(ids[0]);
+      });
+  }
+
+
+function update(id, changes) {
+    return db('class')
+        .where({ id })
+        .update(changes);
+    }
+
+function remove(id) {
+    return db('class')
+        .where('id', id)
+        .del();
+    }
