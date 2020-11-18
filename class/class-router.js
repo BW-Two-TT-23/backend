764c7ee2335d04cbbb9c9ba38e37b1df4d12050e
Christopher Barrett <municipalshock@gmail.com>
2020-11-18 12:35:29 -0800

    update error message

--- class/class-router.js
+++ class/class-router.js
@@ -31,36 +31,33 @@ router.get('/:type', (req, res)=>{
         })
 })
 
-router.post('/', restricted, async (req, res) => {
+router.post('/:id', (req, res) => {
+    const { id } = req.params;
 
-    try {
-        console.log(req.body)
-      const classe = await classDb.addClass(req.body)
-      res.status(200).json(classe);
-    } 
-    catch (error) {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error adding the class to the database',
-      });
-    }
-  });
+    classDb.find()
+        .then( posts =>{
+            res.status(200).json(posts);
+            console.log(posts);
+        })
+        .catch( err=>{
+            res.status(500).json( {message: "something went wrong adding your classs"});
+            console.log(err);
+        })
+})
 
-router.put('/:id', async (req, res) => {
-    try {
-    
-    const classe = await classDb.updateClass(req.params.id, req.body);
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
 
-      res.status(200).json(classe);
-    } catch (error) {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error updating the Class :(',
-      });
-    }
-  });
+    classDb.find()
+        .then( posts =>{
+            res.status(200).json(posts);
+            console.log(posts);
+        })
+        .catch( err=>{
+            res.status(500).json(err);
+            console.log(err);
+        })
+})
 
   router.delete('/:id', async (req, res) => {
     try {
